3d5088d99a9c6632f54910481b3df02bd5adfce9
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-18 14:02:30 -0300

    feat: add Redis microservice and PubSub module for message handling

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { addSwagger } from './app/config/swagger.config';
 import { ValidationPipe } from '@nestjs/common';
+import { addRedisClient } from './app/config/redis.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,5 +20,7 @@ async function bootstrap() {
   await app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  await addRedisClient(configService);
 }
 bootstrap();
